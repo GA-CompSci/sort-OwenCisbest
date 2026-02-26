90fe48b6fc854591d2447b53bd574f45720d42c9
OpieYT <81830938+OpieYT@users.noreply.github.com>
2026-02-26 08:58:56 -0500

    updates to sort for 2/26/26

--- src/Activity1.java
+++ src/Activity1.java
@@ -1,5 +1,48 @@
-public class Activity1 {
+public class Activity1 extends IntegerManager implements PrintPretty {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Activity1 thingy = new Activity1();
+        thingy.buildList();
+        thingy.printPretty();
+    }
+
+    @Override
+    void buildList(){
+        nums = new int[20];
+        for(int i = 0; i < nums.length; i++){
+            int rand = (int)(Math.random() * 100);
+            nums[i] = rand;
+            System.out.println(nums[i]);
+        }
+        
+    }
+
+    public void printPretty(){
+        for(int num : nums){
+            System.out.print(num + " ");
+        }
+    }
+    
+    @Override
+    void shuffle(){
+        //loop thru all nums, index i
+        //pick random spot, r
+        //3 part swap, r-> temp -> i
+    }
+    
+    @Override
+    void insertionSort(){
+    }
+
+    @Override
+    void selectionSort(){
+    }
+
+    @Override
+    void mergeSort(){
+    }
+
+    @Override
+    int pickRandom(){
+        return 0;
     }
 }
